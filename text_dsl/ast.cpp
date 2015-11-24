9f5f989874d13a2233e1bbbf563a8c3be1a6c1be
wangyix <wangyix@gmail.com>
2015-11-24 00:40:42 -0500

    changed AST struct definitions after rethinking language

--- text_dsl/ast.cpp
+++ text_dsl/ast.cpp
@@ -2,26 +2,6 @@
 
 #include <stdio.h>
 
-void Filler::print() const {
-  printf("%d", length);
-  if (shares) {
-    printf("s");
-  }
-  printf("'%c'", c);
-}
-
-void Words::print() const {
-  printf("{w");
-  if (wordSilhouette) {
-    printf("->'%c'", wordSilhouette);
-  }
-  printf(" ");
-  interword.print();
-  printf("}");
-}
-
-// -------------------------------------------------------------------------------------------------
-
 void LiteralLength::print() const {
   printf("%d", value);
   if (shares) {
@@ -39,44 +19,47 @@ void FunctionLength::print() const {
 // -------------------------------------------------------------------------------------------------
 
 void StringLiteral::print() const {
-  length->print();
+  length.print();
   printf("'%s'", str.c_str());
 }
 
 void RepeatedChar::print() const {
-  length->print();
+  length.print();
   printf("'%c'", c);
 }
 
+// -------------------------------------------------------------------------------------------------
+
+void RepeatedCharFuncLength::print() const {
+  length.print();
+  printf("'%c'", c);
+}
+
+void Words::print() const {
+  printf("{w");
+  if (wordSilhouette) {
+    printf("->'%c'", wordSilhouette);
+  }
+  for (const FillerPtr& filler : interwordFillers) {
+    printf(" ");
+    filler->print();
+  }
+  printf("}");
+}
+
 void Block::print() const {
-  length->print();
+  length.print();
   printf("[");
-  for (int i = 0; i <= children.size(); ++i) {
-    if (i == greedyChildIndex) {
-      printf(" ");
-      greedyChild.print();
-    }
-    if (i != children.size()) {
-      printf(" ");
-      children[i]->print();
-    }
+  for (const ASTPtr& child : children) {
+    printf(" ");
+    child->print();
   }
   printf(" ]^");
-  topFiller.print();
+  for (const FillerPtr& filler : topFillers) {
+    filler->print();
+  }
   printf("v");
-  bottomFiller.print();
+  for (const FillerPtr& filler : bottomFillers) {
+    filler->print();
+  }
 }
-
-void Block::addChild(SpecifiedLengthContentPtr child) {
-  children.push_back(std::move(child));
-}
-void Block::addGreedyChild(const Words& words) {
-  assert(greedyChildIndex < 0);
-  greedyChildIndex = children.size();
-  greedyChild = words;
-}
-bool Block::hasGreedyChild() const {
-  return (greedyChildIndex >= 0);
-}
-
-
--- text_dsl/ast.h
+++ text_dsl/ast.h
@@ -7,48 +7,8 @@
 
 typedef int(*LengthFunc)(int);
 
-struct SpecifiedLength;
-struct SpecifiedLengthContent;
-
-typedef std::unique_ptr<SpecifiedLength> SpecifiedLengthPtr;
-typedef std::unique_ptr<SpecifiedLengthContent> SpecifiedLengthContentPtr;
-
-
-struct AST {
-  AST(const char* f_at) : f_at(f_at) {}
-  virtual void print() const = 0;
-  const char* f_at;
-};
-
-// -------------------------------------------------------------------------------------------------
-
-// Used for specifying interword and vertical fillers. It's a RepeatedChar with a literal length.
-struct Filler : public AST {
-  Filler(const char* f_at, int length, bool shares, char c) : AST(f_at),
-    length(length), shares(shares), c(c) {}
-  void print() const override;
-
-  int length;
-  bool shares;
-  char c;
-};
-
-struct Words : public AST {
-  Words(const char* f_at) : AST(f_at), interword(NULL, 1, false, ' '), wordSilhouette('\0') {}
-  Words(const char* f_at, const std::string& source, const Filler& interword, char wordSilhouette = '\0')
-    : AST(f_at), source(source), interword(interword),
-    wordSilhouette(wordSilhouette) {}
-  void print() const override;
-
-  std::string source;
-  Filler interword;
-  char wordSilhouette;        // use '\0' if unused
-};
-
-// -------------------------------------------------------------------------------------------------
-
-struct SpecifiedLength : public AST {
-  SpecifiedLength(const char* f_at, bool shares) : AST(f_at), shares(shares) {}
+struct SpecifiedLength {
+  SpecifiedLength(bool shares) : shares(shares) {}
   virtual ~SpecifiedLength() {}
   virtual void print() const = 0;
 
@@ -56,16 +16,16 @@ struct SpecifiedLength : public AST {
 };
 
 struct LiteralLength : public SpecifiedLength {
-  LiteralLength(const char* f_at, int value, bool shares)
-    : SpecifiedLength(f_at, shares), value(value) {}
+  LiteralLength(int value, bool shares)
+    : SpecifiedLength(shares), value(value) {}
   void print() const override;
 
   int value;
 };
 
 struct FunctionLength : public SpecifiedLength {
-  FunctionLength(const char* f_at, LengthFunc lengthFunc, bool shares)
-    : SpecifiedLength(f_at, shares), lengthFunc(lengthFunc) {}
+  FunctionLength(LengthFunc lengthFunc, bool shares)
+    : SpecifiedLength(shares), lengthFunc(lengthFunc) {}
   void print() const override;
 
   LengthFunc lengthFunc;
@@ -73,47 +33,79 @@ struct FunctionLength : public SpecifiedLength {
 
 // -------------------------------------------------------------------------------------------------
 
-struct SpecifiedLengthContent : public AST {
-  SpecifiedLengthContent(const char* f_at, SpecifiedLengthPtr length)
-    : AST(f_at), length(std::move(length)) {}
-  virtual ~SpecifiedLengthContent() {}
+struct AST {
+  AST(const char* f_at) : f_at(f_at) {}
   virtual void print() const = 0;
-
-  SpecifiedLengthPtr length;
+  
+  const char* f_at;
 };
 
-struct StringLiteral : public SpecifiedLengthContent {
+struct Filler : public AST {
+  Filler(const char* f_at, const LiteralLength& length) : AST(f_at), length(length) {}
+
+  LiteralLength length;
+};
+
+typedef std::unique_ptr<AST> ASTPtr;
+typedef std::unique_ptr<Filler> FillerPtr;
+
+// -------------------------------------------------------------------------------------------------
+
+struct StringLiteral : public Filler {
   StringLiteral(const char* f_at, const std::string& str)
-    : SpecifiedLengthContent(f_at, SpecifiedLengthPtr(new LiteralLength(NULL, (int)str.length(), false))),
-    str(str) {}
+    : Filler(f_at, LiteralLength((int)str.length(), false)), str(str) {}
   void print() const override;
 
   std::string str;
 };
 
-struct RepeatedChar : public SpecifiedLengthContent {
-  RepeatedChar(const char* f_at, SpecifiedLengthPtr length, char c)
-    : SpecifiedLengthContent(f_at, std::move(length)), c(c) {}
+struct RepeatedChar : public Filler {
+  RepeatedChar(const char* f_at, const LiteralLength& length, char c)
+    : Filler(f_at, length), c(c) {}
   void print() const override;
 
   char c;
 };
 
-struct Block : public SpecifiedLengthContent {
-  Block(const char* f_at, SpecifiedLengthPtr length)
-    : SpecifiedLengthContent(f_at, std::move(length)), greedyChildIndex(-1), greedyChild(NULL),
-    topFiller(NULL, 0, false, ' '), bottomFiller(NULL, 0, false, ' ') {}
+// -------------------------------------------------------------------------------------------------
+
+struct RepeatedCharFuncLength : public AST {
+  RepeatedCharFuncLength(const char* f_at, const FunctionLength& length, char c)
+    : AST(f_at), length(length), c(c) {}
   void print() const override;
 
-  void addChild(SpecifiedLengthContentPtr child);
-  void addGreedyChild(const Words& words);
+  FunctionLength length;
+  char c;
+};
+
+struct Words : public AST {
+  Words(const char* f_at, const std::string& source, char wordSilhouette = '\0')
+    : AST(f_at), source(source), wordSilhouette(wordSilhouette) {}
+  void print() const override;
+
+  std::string source;
+  std::vector<FillerPtr> interwordFillers;
+  char wordSilhouette;        // use '\0' if unused
+};
+
+typedef std::unique_ptr<Words> WordsPtr;
+
+struct Block : public AST {
+  Block(const char* f_at, const LiteralLength& length)
+    : AST(f_at), length(length), greedyChildIndex(-1) {}
+  void print() const override;
+
+  void addChild(ASTPtr child);
+  void addGreedyChild(WordsPtr words);
   bool hasGreedyChild() const;
 
-  std::vector<SpecifiedLengthContentPtr> children;
+  LiteralLength length;
+  std::vector<ASTPtr> children;
   int greedyChildIndex;   // use value < 0 if no greedy child
-  Words greedyChild;
-  Filler topFiller;
-  Filler bottomFiller;
+  std::vector<FillerPtr> topFillers;
+  std::vector<FillerPtr> bottomFillers;
 };
+
+// -------------------------------------------------------------------------------------------------
 
 #endif
