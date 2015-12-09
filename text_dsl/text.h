812ae688dfc71186196d316af06fd258b9c9e88a
wangyix <wangyix@gmail.com>
2015-12-08 17:49:50 -0800

    added =NULL for wordSources and lengthFuncs args

--- text_dsl/text.h
+++ text_dsl/text.h
@@ -10,10 +10,10 @@
 int sprintf(std::string* str, const char* format, ...);
 int vsprintf(std::string* str, const char* format, va_list args);
 
-void text_printf(const char* format, const char** wordSources, const LengthFunc* lengthFuncs, ...);
-void text_fprintf(FILE* stream, const char* format, const char** wordSources, const LengthFunc* lengthFuncs, ...);
-void text_sprintf(std::string* str, const char* format, const char** wordSources, const LengthFunc* lengthFuncs, ...);
-void text_sprintf_lines(std::vector<std::string>* lines, const char* format, const char** wordSources, const LengthFunc* lengthFuncs, ...);
-void text_sprintf_lines_append(std::vector<std::string>* lines, const char* format, const char** wordSources, const LengthFunc* lengthFuncs, ...);
+void text_printf(const char* format, const char** wordSources=NULL, const LengthFunc* lengthFuncs=NULL, ...);
+void text_fprintf(FILE* stream, const char* format, const char** wordSources=NULL, const LengthFunc* lengthFuncs=NULL, ...);
+void text_sprintf(std::string* str, const char* format, const char** wordSources=NULL, const LengthFunc* lengthFuncs=NULL, ...);
+void text_sprintf_lines(std::vector<std::string>* lines, const char* format, const char** wordSources=NULL, const LengthFunc* lengthFuncs=NULL, ...);
+void text_sprintf_lines_append(std::vector<std::string>* lines, const char* format, const char** wordSources=NULL, const LengthFunc* lengthFuncs=NULL, ...);
 
 #endif
